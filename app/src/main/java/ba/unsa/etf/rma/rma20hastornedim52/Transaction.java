df3d223cc9dcd4e54ba62884f2ac6543f8e73370
nhastor1 <nhastor1@etf.unsa.ba>
2020-03-23 00:24:39 +0100

    Created Transaction class and TransactionType enum

---
+++ app/src/main/java/ba/unsa/etf/rma/rma20hastornedim52/Transaction.java
@@ -0,0 +1,87 @@
+package ba.unsa.etf.rma.rma20hastornedim52;
+
+import java.util.Date;
+
+public class Transaction {
+    private Date date;
+    private double amount;
+    private String title;
+    private TransactionType type;
+    private String itemDescription;
+    private int transactionInterval;
+    private Date endDate;
+
+    public Transaction(Date date, double amount, String title, TransactionType type, String itemDescription, int transactionInterval, Date endDate) {
+        this.date = date;
+        this.amount = amount;
+        setTitle(title);
+        this.type = type;
+
+        if(!(type.equals(TransactionType.INDIVIDUALINCOME) || type.equals(TransactionType.REGULARINCOME)))
+            this.itemDescription = itemDescription;
+
+        if(type.equals(TransactionType.REGULARINCOME) || type.equals(TransactionType.REGULARPAYMENT))
+            this.transactionInterval = transactionInterval;
+
+        if(type.equals(TransactionType.REGULARPAYMENT) || type.equals(TransactionType.REGULARINCOME))
+            this.endDate = endDate;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if(title.length()<=3 || title.length()>=15)
+            throw new IllegalArgumentException("Transaction title is not regular");
+        this.title = title;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public int getTransactionInterval() {
+        return transactionInterval;
+    }
+
+    public void setTransactionInterval(int transactionInterval) {
+        this.transactionInterval = transactionInterval;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
---
+++ app/src/main/java/ba/unsa/etf/rma/rma20hastornedim52/TransactionType.java
@@ -0,0 +1,5 @@
+package ba.unsa.etf.rma.rma20hastornedim52;
+
+public enum TransactionType {
+    INDIVIDUALPAYMENT, REGULARPAYMENT, PURCHASE, INDIVIDUALINCOME, REGULARINCOME
+}
