17f60428e51e7d29471a3c0bebd764a8746938c4
nhastor1 <nhastor1@etf.unsa.ba>
2020-03-22 23:51:11 +0100

    Initial commit

---
+++ app/src/main/java/ba/unsa/etf/rma/rma20hastornedim52/MainActivity.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rma.rma20hastornedim52;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
