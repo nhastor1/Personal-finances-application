0892dd3f41899fb6f3c9c069c4cf46f8c75ba15b
nhastor1 <nhastor1@etf.unsa.ba>
2020-04-11 17:07:20 +0200

    Chart fragment created

--- app/src/main/java/ba/unsa/etf/rma/rma20hastornedim52/Graphs/GraphsFragment.java
+++ app/src/main/java/ba/unsa/etf/rma/rma20hastornedim52/Graphs/GraphsFragment.java
@@ -4,13 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ba.unsa.etf.rma.rma20hastornedim52.Adapter.FilterSpinnerAdapter;
 import ba.unsa.etf.rma.rma20hastornedim52.Budget.BudgetFragment;
 import ba.unsa.etf.rma.rma20hastornedim52.OnSwipeTouchListener;
 import ba.unsa.etf.rma.rma20hastornedim52.R;
@@ -18,7 +28,15 @@ import ba.unsa.etf.rma.rma20hastornedim52.R;
 public class GraphsFragment extends Fragment {
 
     private View view;
-    private BarChart chart;
+
+    private Spinner spinner;
+    private ArrayAdapter<String> spinerAdapter;
+
+    private BarChart chartPayment;
+    private BarChart chartIncome;
+    private BarChart chartAll;
+
+
 
     @Nullable
     @Override
@@ -41,7 +59,49 @@ public class GraphsFragment extends Fragment {
             }
         });
 
+        // Setting spinnerChartInterval
+        spinner = (Spinner) view.findViewById(R.id.spinnerChartInterval);
+
+        List<String> chartInterval = new ArrayList<String>(){
+            {
+                add("Days");
+                add("Weeks");
+                add("Months");
+            }
+        };
+        spinerAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item, chartInterval);
+        //spinerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinerAdapter);
+
+        chartPayment = (BarChart) view.findViewById(R.id.chartPayment);
+        chartIncome = (BarChart) view.findViewById(R.id.chartIncome);
+        chartAll = (BarChart) view.findViewById(R.id.chartAll);
+
+        BarDataSet barDataSet = new BarDataSet(dataValues1(), "chartAll");
+
+        BarData barData = new BarData();
+        barData.addDataSet(barDataSet);
+
+        chartPayment.setData(barData);
+        chartPayment.invalidate();
+
+        chartIncome.setData(barData);
+        chartIncome.invalidate();
+
+        chartAll.setData(barData);
+        chartAll.invalidate();
+
         return view;
+    }
+
+    private ArrayList<BarEntry> dataValues1(){
+        ArrayList<BarEntry> dataValues = new ArrayList<>();
+        dataValues.add(new BarEntry(0, 1));
+        dataValues.add(new BarEntry(1, 2));
+        dataValues.add(new BarEntry(2, 5));
+        dataValues.add(new BarEntry(3, 1));
+        dataValues.add(new BarEntry(5, 7));
+        return dataValues;
     }
 
     private void finish() {
